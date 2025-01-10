26fc05e0ae7f96d4d19009412d726dedce866032
Ajay-kannan2006 <ajaykannan1001200635@gmail.com>
2025-01-10 08:45:06 +0530

    Javascript data types completed

---
+++ Day_4/Javascript/prac.js
@@ -0,0 +1,28 @@
+console.log("hello world");
+a = 1;
+b = 'a';
+c = "string";
+console.log(a);
+console.log(b);
+console.log(c);
+
+//object declarations
+//1st
+obj = { firstname: "sri", lastname: "Eshwar" }
+console.log(obj)
+//2nd
+obj1 = {};
+console.log(obj1)
+
+//3rd
+obj2 = new Object();
+obj2.count = 70;
+obj2.abs = 1;
+console.log(obj2);
+
+//Set
+set = new Set(["hello", 2, "SECE"], 4);
+console.log(typeof (set));
+set.add(1);
+set.add(1);
+console.log(set);
