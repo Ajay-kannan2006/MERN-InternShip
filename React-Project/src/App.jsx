d07e8ee276e115fa400125806c752743b5d15146
Ajay-kannan2006 <ajaykannan1001200635@gmail.com>
2025-01-22 09:13:33 +0530

    log in page completed using router

--- React-Project/src/App.jsx
+++ React-Project/src/App.jsx
@@ -1,21 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import ClassComp from './ClassComp'
-import Gallery from './Gallery'
-import Contact from './Contact'
+import Home from "./components/functionalComponets/Home"
+import Button from "./components/functionalComponets/Button";
+import Card from "./components/functionalComponets/Card";
+import Footer from "./components/functionalComponets/Footer";
+import Header from "./components/functionalComponets/Header";
+import Profile from "./components/functionalComponets/Profile";
+import ClassCompEG from "./components/classComponents/classCompEG";
+import Gallery from "./components/classComponents/Gallery";
+import About from "./components/functionalComponets/About";
+import Contact from "./components/functionalComponets/contact";
+import Navbar from "./components/functionalComponets/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import SignUp from "./components/functionalComponets/SignUp";
 
-function App() {
-  // const [count, setCount] = useState(0)
-
+const App = () => {
   return (
     <>
-      <ClassComp />
-      <Gallery image="SECE Logo" />
-      <Contact />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<SignUp />}></Route>
+          <Route path="/home" element={<Home />}></Route>
+          <Route path="/card" element={<Card title="Introduction" description="This is a simple card component." />}></Route>
+          <Route path="/about" element={<About Hello="Geethapriyan" />}></Route> {/* props Example */}
+          <Route path="/profile" element={<Profile name="Geethapriyan S" age={20} profession="Developer" />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>   {/* state Example */}
+        </Routes>
+      </BrowserRouter>
+      <Footer />
+      {/* <Button label="Click Me" onClick={() => alert("Button clicked!")} /> */}
     </>
-  )
-}
+  );
+};
 
-export default App
+export default App;
---
+++ React-Project/src/components/functionalComponets/Navbar.jsx
@@ -0,0 +1,20 @@
+ import '../../css/Navbar.css'
+ import { Link } from "react-router-dom"
+ 
+ const Navbar = () =>{
+    return (
+     <header>
+         <nav>
+             <ol className='navbar'>
+                 <li><Link to='/home' className='link'>Home</Link></li>
+                 <li><Link to='/card' className='link'>Card</Link></li>
+                 <li><Link to='/about' className='link'>About</Link></li>
+                 <li><Link to='/profile' className='link'>Profile</Link></li>
+                 <li><Link to='/contact' className='link'>Contact</Link></li>
+             </ol>
+         </nav>
+     </header>
+    );
+ };
+ export default Navbar;
+
