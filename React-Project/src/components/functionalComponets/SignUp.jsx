5d08b82216a7e3b96b0c903eb47104bc102c063d
Ajay-kannan2006 <ajaykannan1001200635@gmail.com>
2025-01-29 09:02:57 +0530

    frontedn and backend connected

--- React-Project/src/components/functionalComponets/SignUp.jsx
+++ React-Project/src/components/functionalComponets/SignUp.jsx
@@ -1,25 +1,25 @@
 import React from 'react'
 import "../../css/SignUp.css"
-import {Link} from "react-router-dom"
+import { Link } from "react-router-dom"
 
-const SignUp = ()=>{
-  return(
-    <div className="body">
-    <div className="center">
+const SignUp = () => {
+   return (
+      <div className="body">
+         <div className="center">
             <div class="text">
                Login Form
             </div>
-            <form action="#">
+            <form action="http://localhost:">
                <div className="data">
                   <label>Email or Phone</label>
-                  <input type="text" required/>
+                  <input type="text" required />
                </div>
                <div className="data">
                   <label>Password</label>
-                  <input type="password" required/>
+                  <input type="password" required />
                </div>
                <div className="forgot-pass">
-                  <a href="#">Forgot Password?</a> 
+                  <a href="#">Forgot Password?</a>
                </div>
                <div className="btn">
                   <div className="inner"></div>
@@ -30,7 +30,7 @@ const SignUp = ()=>{
                </div>
             </form>
          </div>
-         </div>
-  )
+      </div>
+   )
 }
 export default SignUp;
